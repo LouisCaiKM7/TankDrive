c39bf3a9206f1c2082f7f93a3d05ca85793f5667
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-08-31 21:02:28 +0800

    create README.md

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Tank";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-08-31 20:54:53 CST";
-  public static final long BUILD_UNIX_TIME = 1756644893798L;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "63169f94d60cc5bf40df9b6fce584d988824ee3b";
+  public static final String GIT_DATE = "2025-08-31 20:58:43 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-08-31 20:59:50 CST";
+  public static final long BUILD_UNIX_TIME = 1756645190671L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/README.md
@@ -0,0 +1,5 @@
+Problems analysis:
+can't stop after pressed s, j and l keys, won't react when pressing w key.(single use drivetrain haha)
+ntparams keep refreshing because the isanyChanged is always true , need to fix this.
+not verified poseestimator and odometry.
+not verified commands
