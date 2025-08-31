d2d457abfb413c29c6757c355b9e47ccf2a449f3
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-08-31 21:58:06 +0800

    remove unused imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,32 +1,17 @@
 package frc.robot;
 
 import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.TankDrive.TankCommands;
-import frc.robot.TankDrive.TankConfigs;
-import frc.robot.TankDrive.TankLimits;
 import frc.robot.TankDrive.TankSubsystem;
 import frc.robot.TankDrive.Side.Side;
-import frc.robot.TankDrive.Side.SideIO;
 import frc.robot.TankDrive.Side.SideIOReal;
 import frc.robot.TankDrive.Side.SideIOSim;
 import frc.robot.TankDrive.Imu.ImuIOPigeon2;
 import frc.robot.TankDrive.Imu.ImuIOSim;
-import frc.robot.TankDrive.TankConfigs.SideConfigsSim;
 import frc.robot.TankDrive.TankSubsystem.TankDrivingStates;
-
-import java.util.ArrayList;
-import java.util.Optional;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -35,8 +20,6 @@ public class RobotContainer {
     /* ---------- Joysticks ---------- */
     private final CommandXboxController controller = new CommandXboxController(0);
     private TankSubsystem tankSubsystem;
-    private Side left;
-    private Side right;
 
     private void configureSubsystems() {
         if(Robot.isReal()){
