03b3facec2eff6b62e88bd372dc5660186b08ebe
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-08-31 21:05:48 +0800

    format

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Problems analysis:
+
 can't stop after pressed s, j and l keys, won't react when pressing w key.(single use drivetrain haha)
+
 ntparams keep refreshing because the isanyChanged is always true , need to fix this.
+
 not verified poseestimator and odometry.
+
 not verified commands
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Tank";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "63169f94d60cc5bf40df9b6fce584d988824ee3b";
-  public static final String GIT_DATE = "2025-08-31 20:58:43 CST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "02772a55ea1db80a05e98e3df45bba11befb6fed";
+  public static final String GIT_DATE = "2025-08-31 21:03:25 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-08-31 20:59:50 CST";
-  public static final long BUILD_UNIX_TIME = 1756645190671L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-08-31 21:03:56 CST";
+  public static final long BUILD_UNIX_TIME = 1756645436000L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
